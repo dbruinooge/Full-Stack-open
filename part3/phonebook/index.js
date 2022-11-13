3eb48da40a9cc7dad6190b76d775bce11034d2af
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-11-13 09:37:35 +0900

    Add part 3 exercises

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-const phonebook = [
+let phonebook = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -24,6 +25,28 @@ const phonebook = [
   }
 ];
 
+function generateId() {
+  const maxId = phonebook.length > 0
+    ? Math.max(...phonebook.map(person => person.id))
+    : 0
+  return maxId + 1;
+}
+
+app.use(express.json());
+
+app.use(morgan((tokens, req, res) => {
+  let person = { name: req.body.name, number: req.body.number };
+  console.log(req.body);
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(person)
+  ].join(' ');
+}));
+
 app.get('/info', (request, response) => {
   const requestTime = new Date().toString();
   response.send(`<p>Phonebook has info for ${phonebook.length} people</p>
@@ -38,7 +61,31 @@ app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   const person = phonebook.find(entry => entry.id === id);
   response.json(person);
-})
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  phonebook = phonebook.filter(entry => entry.id !== id);
+  response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body;
+  if (!person.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  } else if (phonebook.find(entry => entry.name === person.name)) {
+    return response.status(400).json({
+      error: 'name already exists in phonebook'
+    })
+  } else {
+    person.id = generateId();
+    phonebook.push(person);
+    response.json(person);
+  }
+
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
